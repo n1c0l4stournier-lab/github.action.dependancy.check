77beaf1c370db0b7e8df6e474511c227f25ad4ae
Nicolas Tournier <dr.nicolas.tournier@gmail.com>
2021-04-06 11:27:59 +0200

    Dependancy check (#1)
    * init: dependancy check suite
    
    * init: dependancy check docker build
    
    * init: dependancy check docker delivery
    
    * init: dependancy check docker container structure test
    
    * refactor: clear configuration and infra outside
    
    * fix: gitlabci

---
+++ README.md
@@ -0,0 +1,2 @@
+https://developers.redhat.com/blog/2019/02/26/create-java-8-runtime-container-image/
+https://code.quarkus.io/
--- config/omnibus_config.rb
+++
@@ -1,2 +0,0 @@
-external_url 'http://10.1.1.1'
-gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password')
